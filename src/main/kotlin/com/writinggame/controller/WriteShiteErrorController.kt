56aa76f45a6241291c84e8e92a01183d05078c86
Julian Jocque <lamabuny@gmail.com>
2020-09-20 15:11:16 -0400

    Handle 404s by redirecting to index

---
+++ src/main/kotlin/com/writinggame/controller/WriteShiteErrorController.kt
@@ -0,0 +1,27 @@
+package com.writinggame.controller
+
+import org.springframework.boot.web.servlet.error.ErrorController
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import javax.servlet.http.HttpServletRequest
+
+//Crappy name because ErrorController is in use
+@Controller
+class WriteShiteErrorController : ErrorController {
+
+    /**
+     * Redirect all errors to index. This handles any 404s.
+     */
+    @RequestMapping("/error")
+    @ResponseBody
+    fun handleError(request: HttpServletRequest): String {
+        return "<head>" +
+                "  <meta http-equiv=\"refresh\" content=\"0; URL=/\" />" +
+                "</head>"
+    }
+
+    override fun getErrorPath(): String {
+        return "/error"
+    }
+}
