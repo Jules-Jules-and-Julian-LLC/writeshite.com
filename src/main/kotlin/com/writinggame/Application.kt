795b8aaeee93cc53e8ef931db3055efe094bd06a
Julian Jocque <lamabuny@gmail.com>
2020-09-24 20:30:51 -0400

    Change server port to 80

--- src/main/kotlin/com/writinggame/Application.kt
+++ src/main/kotlin/com/writinggame/Application.kt
@@ -2,11 +2,18 @@ package com.writinggame
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import java.util.*
 
 @SpringBootApplication
 class Application
 
 fun main(args : Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
+    val properties = Properties()
+    properties.setProperty("server.port", "80")
+
+    val app = SpringApplication(Application::class.java)
+    app.setDefaultProperties(properties)
+
+    app.run(*args)
 }
 
