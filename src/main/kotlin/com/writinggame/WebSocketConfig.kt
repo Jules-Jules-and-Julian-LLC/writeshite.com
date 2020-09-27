448409ff8d9735a9bef1deb37a3ad90ff85aa314
Julian Jocque <lamabuny@gmail.com>
2020-09-27 18:44:03 -0400

    Redirect HTTP requests to HTTPS

--- src/main/kotlin/com/writinggame/WebSocketConfig.kt
+++ src/main/kotlin/com/writinggame/WebSocketConfig.kt
@@ -2,7 +2,13 @@ package com.writinggame
 
 import com.writinggame.controller.viewModels.JoinGameResponse
 import com.writinggame.model.LobbyManager
+import org.apache.catalina.Context
+import org.apache.catalina.connector.Connector
+import org.apache.tomcat.util.descriptor.web.SecurityCollection
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.event.EventListener
 import org.springframework.messaging.simp.SimpMessageSendingOperations
@@ -34,16 +40,47 @@ class WebSocketConfig : WebSocketMessageBrokerConfigurer {
     @EventListener
     fun onSocketDisconnected(event: SessionDisconnectEvent) {
         val sha = StompHeaderAccessor.wrap(event.message)
-        if(sha.sessionId != null) {
+        if (sha.sessionId != null) {
             val receivedDatetime = ZonedDateTime.now()
             val lobby = LobbyManager.leaveLobby(sha.sessionId!!)
-            if(lobby != null) {
+            if (lobby != null) {
                 messagingTemplate.convertAndSend(
-                    "/topic/lobby.${lobby.lobbyId}",
-                    JoinGameResponse(lobby, receivedDatetime)
+                        "/topic/lobby.${lobby.lobbyId}",
+                        JoinGameResponse(lobby, receivedDatetime)
                 )
             }
         }
         println("[Disconnected] " + sha.sessionId)
+    }
+
+    @Bean
+    fun servletContainer(): TomcatServletWebServerFactory {
+        val httpConnector1 = Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL)
+        httpConnector1.scheme = "http"
+        httpConnector1.port = 8080
+        httpConnector1.secure = false
+        httpConnector1.redirectPort = 443
+
+        val httpConnector2 = Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL)
+        httpConnector2.scheme = "http"
+        httpConnector2.port = 80
+        httpConnector2.secure = false
+        httpConnector2.redirectPort = 443
+
+        val tomcat = object : TomcatServletWebServerFactory() {
+            override fun postProcessContext(context: Context?) {
+                super.postProcessContext(context)
+                val securityConstraint = SecurityConstraint()
+                securityConstraint.userConstraint = "CONFIDENTIAL"
+
+                val collection = SecurityCollection()
+                collection.addPattern("/*")
+                securityConstraint.addCollection(collection)
+                context?.addConstraint(securityConstraint)
+            }
+        }
+        tomcat.addAdditionalTomcatConnectors(httpConnector1, httpConnector2)
+
+        return tomcat;
     }
 }
