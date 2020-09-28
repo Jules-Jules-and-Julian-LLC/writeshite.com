a91fa0ff4bf7e38330401b25cd0ac2fc47b09db9
Julian Jocque <lamabuny@gmail.com>
2020-09-27 20:39:13 -0400

    Default to 10 word minimum, but it didn't seem to work

--- src/main/kotlin/com/writinggame/model/GameSettings.kt
+++ src/main/kotlin/com/writinggame/model/GameSettings.kt
@@ -2,6 +2,6 @@ package com.writinggame.model
 
 import com.writinggame.domain.StoryPassStyleType
 
-class GameSettings(val roundTimeMinutes: Long? = 10, val minWordsPerMessage: Int? = null, val maxWordsPerMessage: Int? = null,
+class GameSettings(val roundTimeMinutes: Long? = 10, val minWordsPerMessage: Int? = 10, val maxWordsPerMessage: Int? = null,
     val passStyle: StoryPassStyleType = StoryPassStyleType.MINIMIZE_WAIT
 )
