84c73a431446a082a76982fbfcea1e23903b5004
Julian Jocque <lamabuny@gmail.com>
2020-09-30 21:08:39 -0400

    Fix websocket in compose container

--- src/main/kotlin/com/writinggame/Application.kt
+++ src/main/kotlin/com/writinggame/Application.kt
@@ -2,19 +2,11 @@ package com.writinggame
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import java.util.*
 
 @SpringBootApplication
 class Application
 
 fun main(args : Array<String>) {
-//    SpringApplication.run(Application::class.java, *args)
-    val properties = Properties()
-    properties.setProperty("server.port", "1337")
-
-    val app = SpringApplication(Application::class.java)
-    app.setDefaultProperties(properties)
-
-    app.run(*args)
+    SpringApplication.run(Application::class.java, *args)
 }
 
