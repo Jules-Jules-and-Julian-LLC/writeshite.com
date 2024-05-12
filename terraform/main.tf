86578097ebe25f2b346097adb8e38983df33a976
Julian Jocque <lamabuny@gmail.com>
2024-05-12 12:43:26 -0400

    definitely interesting development

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = "~> 1.8.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -9,64 +9,66 @@ terraform {
 }
 
 provider "google" {
-  project = "peaceful-app-379819"
-  region  = "us-central1"
+  project = var.project_id
+  region  = var.region
 }
 
-resource "google_storage_bucket" "artifact_bucket" {
-  name     = "writeshite-artifacts"
-  location = "us-central1"
+resource "google_service_account" "service_account" {
+  account_id   = "writeshite-service-account"
+  display_name = "WriteShite Service Account"
+}
+
+resource "google_project_iam_member" "service_account_roles" {
+  project = var.project_id
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+
+  for_each = toset([
+    "roles/storage.admin",
+    "roles/dns.admin",
+    "roles/run.admin",
+    "roles/cloudbuild.builds.editor",
+  ])
 }
 
 resource "google_cloud_run_service" "backend" {
   name     = "writeshite-backend"
-  location = "us-central1"
+  location = var.region
 
   template {
     spec {
       containers {
-        image = "gcr.io/peaceful-app-379819/writeshite-backend:latest"
-        env {
-          name  = "ENV_VARIABLE_NAME"
-          value = "ENV_VARIABLE_VALUE"
-        }
+        image = "gcr.io/${var.project_id}/writeshite-backend:latest"
       }
     }
   }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
 }
 
-resource "google_storage_bucket" "frontend_bucket" {
-  name     = "whiteshite-frontend-bucket"
-  location = "us-central1"
+resource "google_storage_bucket" "frontend" {
+  name          = "writeshite-frontend"
+  location      = var.region
+  force_destroy = true
 
   uniform_bucket_level_access = true
 
   website {
     main_page_suffix = "index.html"
-    not_found_page   = "index.html"
+    not_found_page   = "404.html"
   }
 }
 
-resource "google_cloud_run_domain_mapping" "backend_domain_mapping" {
-  location = "us-central1"
-  name     = "writeshite.com"
-
-  metadata {
-    namespace = "peaceful-app-379819"
-  }
-
-  spec {
-    route_name = google_cloud_run_service.backend.name
-  }
+resource "google_dns_managed_zone" "writeshite_zone" {
+  name        = "writeshite-zone"
+  dns_name    = "writeshite.com."
+  description = "DNS zone for writeshite.com"
 }
 
-resource "google_storage_bucket_iam_member" "frontend_domain_mapping" {
-  bucket = google_storage_bucket.frontend_bucket.name
-  role   = "roles/storage.objectViewer"
-  member = "allUsers"
+resource "google_dns_record_set" "writeshite_record" {
+  name = "www.${google_dns_managed_zone.writeshite_zone.dns_name}"
+  type = "CNAME"
+  ttl  = 300
+
+  managed_zone = google_dns_managed_zone.writeshite_zone.name
+
+  rrdatas = [google_cloud_run_service.backend.status[0].url]
 }
