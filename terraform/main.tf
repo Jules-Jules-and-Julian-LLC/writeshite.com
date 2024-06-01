6dd6e50692e99f00d7c24382a15e91f5c991e9d7
Julian Jocque <lamabuny@gmail.com>
2024-06-01 19:49:07 -0400

    Remove networking stuff

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.8.0"
+  required_version = "~> 1.7.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -18,21 +18,36 @@ resource "google_service_account" "service_account" {
   display_name = "WriteShite Service Account"
 }
 
-resource "google_storage_bucket" "writeshite-frontend" {
-  name          = "writeshite-frontend-bucket"
-  location      = "us-central1"
+resource "google_storage_bucket" "frontend_bucket" {
+  name     = "whiteshite-frontend"
+  location = var.region
+  project = var.project_id
 
   website {
     main_page_suffix = "index.html"
     not_found_page   = "404.html"
   }
   cors {
-    origin          = ["https://writeshite.com", "https://www.writeshite.com",
-      "http://writeshite.com", "http://www.writeshite.com"]
+    origin = [
+      "https://writeshite.com", "https://www.writeshite.com",
+      "http://writeshite.com", "http://www.writeshite.com", "writeshite.com", "www.writeshite.com"
+    ]
     method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
     response_header = ["*"]
     max_age_seconds = 3600
   }
+}
+
+resource "google_storage_bucket" "writeshite-tfstate" {
+  name     = "writeshite-tfstate"
+  location = var.region
+  project  = var.project_id
+}
+
+resource "google_storage_bucket" "artifact_bucket" {
+  name     = "writeshite-artifacts"
+  location = var.region
+  project  = var.project_id
 }
 
 resource "google_project_iam_member" "service_account_roles" {
@@ -42,7 +57,6 @@ resource "google_project_iam_member" "service_account_roles" {
 
   for_each = toset([
     "roles/storage.admin",
-    "roles/bucket.admin",
     "roles/dns.admin",
     "roles/run.admin",
     "roles/cloudbuild.builds.editor",
@@ -62,31 +76,43 @@ resource "google_cloud_run_service" "backend" {
   }
 }
 
-resource "google_storage_bucket" "frontend" {
-  name          = "writeshite-frontend"
-  location      = var.region
-  force_destroy = true
-
-  uniform_bucket_level_access = true
-
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-}
-
-resource "google_dns_managed_zone" "writeshite_zone" {
-  name        = "writeshite-zone"
-  dns_name    = "writeshite.com."
-  description = "DNS zone for writeshite.com"
-}
-
-resource "google_dns_record_set" "writeshite_record" {
-  name = "www.${google_dns_managed_zone.writeshite_zone.dns_name}"
-  type = "CNAME"
-  ttl  = 300
-
-  managed_zone = google_dns_managed_zone.writeshite_zone.name
-
-  rrdatas = [google_cloud_run_service.backend.status[0].url]
-}
+#Networking stuff already done by hand
+#resource "google_dns_managed_zone" "writeshite_zone" {
+#  name        = "writeshite-zone"
+#  dns_name    = "writeshite.com."
+#  description = "DNS zone for writeshite.com"
+#}
+#
+#resource "google_dns_record_set" "writeshite_record" {
+#  name = "www.${google_dns_managed_zone.writeshite_zone.dns_name}"
+#  type = "CNAME"
+#  ttl  = 300
+#
+#  managed_zone = google_dns_managed_zone.writeshite_zone.name
+#
+#  rrdatas = ["writeshite.com"]
+#}
+#
+#resource "google_storage_bucket_iam_member" "frontend_domain_mapping" {
+#  bucket = google_storage_bucket.frontend_bucket.id
+#  member = "allUsers"
+#  role   = "roles/storage.objectViewer"
+#}
+#
+#resource google_cloud_run_domain_mapping "backend_domain_mapping" {
+#  location = var.region
+#  project = var.project_id
+#  name = "writeshite.com"
+#  spec {
+#    route_name = google_cloud_run_service.backend.name
+#  }
+#  metadata {
+#    namespace = var.project_id
+#  }
+#}
+#
+#resource "google_storage_bucket_iam_member" "backend_domain_mapping" {
+#  bucket = google_storage_bucket.artifact_bucket.id
+#  member = "allUsers"
+#  role   = "roles/storage.objectViewer"
+#}
