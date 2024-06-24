30ce494588d56fe33fc4782d44d158587b28e642
Julian Jocque <lamabuny@gmail.com>
2024-06-24 12:44:32 -0400

    Switch to env variables for secrets, fix globalscope warning

--- src/main/kotlin/com/writinggame/model/GalleryManager.kt
+++ src/main/kotlin/com/writinggame/model/GalleryManager.kt
@@ -8,29 +8,32 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.io.File
+import kotlinx.coroutines.*
 import java.util.*
+
+private const val BUCKET_NAME = "write-shite-galleries"
 
 object GalleryManager {
     private val jsonObjectMapper: ObjectMapper = jacksonObjectMapper().findAndRegisterModules()
     private val s3Client = initAmazonS3Client()
+    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
 
     private fun initAmazonS3Client(): AmazonS3 {
-        val rawCreds: AwsCredentials = jsonObjectMapper.readValue(File("secrets/aws_creds.json"))
-        val credential = BasicAWSCredentials(rawCreds.accessKey, rawCreds.secretKey)
+        val accessKey = System.getenv("B2_ACCESS_KEY") ?: throw IllegalStateException("B2_ACCESS_KEY is not set")
+        val secretKey = System.getenv("B2_SECRET_KEY") ?: throw IllegalStateException("B2_SECRET_KEY is not set")
+        val endpoint = System.getenv("B2_ENDPOINT") ?: throw IllegalStateException("B2_ENDPOINT is not set")
+
+        val credentials = BasicAWSCredentials(accessKey, secretKey)
 
         return AmazonS3ClientBuilder.standard()
-            .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(rawCreds.endpoint, "us-west-002"))
-            .withCredentials(AWSStaticCredentialsProvider(credential)).build()
+            .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(endpoint, "us-west-002"))
+            .withCredentials(AWSStaticCredentialsProvider(credentials)).build()
     }
 
-    fun loadFromFile(lobbyId: String) : Gallery {
-        if(s3Client.doesObjectExist("write-shite-galleries", "${lobbyId.uppercase(Locale.getDefault())}.json")) {
-            val existingFile = s3Client.getObject("write-shite-galleries",
+    fun loadFromFile(lobbyId: String): Gallery {
+        if (s3Client.doesObjectExist(BUCKET_NAME, "${lobbyId.uppercase(Locale.getDefault())}.json")) {
+            val existingFile = s3Client.getObject(
+                BUCKET_NAME,
                 "${lobbyId.uppercase(Locale.getDefault())}.json")
             return jsonObjectMapper.readValue(existingFile.objectContent)
         }
@@ -39,11 +42,10 @@ object GalleryManager {
     }
 
     fun addStoriesToGallery(lobbyId: String, stories: List<Story>) {
-        GlobalScope.launch {
+        scope.launch {
             withContext(Dispatchers.IO) {
                 val galleryToSave = loadFromFile(lobbyId)
                 galleryToSave.addStories(stories)
-                //Warning is not true, because of withContext(Dispatchers.IO). Technically, it blocks but it'll make new threads as needed
                 s3Client.putObject("write-shite-galleries",
                     "${lobbyId.uppercase(Locale.getDefault())}.json", jsonObjectMapper.writeValueAsString(galleryToSave))
             }
