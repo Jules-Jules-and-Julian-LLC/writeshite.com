4102d3826d8a234c87f0c89a1a931ac215988ff3
Julian Jocque <lamabuny@gmail.com>
2024-08-09 19:18:15 -0400

    Add explicit gallery route

--- src/main/kotlin/com/writinggame/controller/GalleryController.kt
+++ src/main/kotlin/com/writinggame/controller/GalleryController.kt
@@ -6,10 +6,19 @@ import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletRequestAttributes
 
 @RestController
 @CrossOrigin(origins = ["http://localhost:3000", "https://localhost", "https://www.writeshite.com", "https://writeshite.com"])
 class GalleryController {
+    @GetMapping("/gallery/{lobbyId}/")
+    fun getGallery(@PathVariable("lobbyId") lobbyId: String) : String {
+        val path = (RequestContextHolder.currentRequestAttributes() as ServletRequestAttributes).request.requestURI
+        println("GET gallery/${lobbyId}/, page call")
+        println("Redirecting gallery $path to index.html")
+        return "forward:/index.html"
+    }
 
     @GetMapping("/gallery/{lobbyId}/get")
     fun getGalleryJson(@PathVariable("lobbyId") lobbyId: String) : Gallery {
