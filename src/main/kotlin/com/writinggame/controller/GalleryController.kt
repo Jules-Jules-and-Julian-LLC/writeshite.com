0fa71b0fb1ba10dad8b3152f6c985c582bf135a0
Julian Jocque <lamabuny@gmail.com>
2024-08-30 13:35:52 -0400

    Swap to RequestMapping

--- src/main/kotlin/com/writinggame/controller/GalleryController.kt
+++ src/main/kotlin/com/writinggame/controller/GalleryController.kt
@@ -2,17 +2,15 @@ package com.writinggame.controller
 
 import com.writinggame.model.Gallery
 import com.writinggame.model.GalleryManager
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.context.request.RequestContextHolder
-import org.springframework.web.context.request.ServletRequestAttributes
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @CrossOrigin(origins = ["http://localhost:3000", "https://localhost", "https://www.writeshite.com", "https://writeshite.com"])
 class GalleryController {
-    @GetMapping("/rawJson/{lobbyId}")
+    @RequestMapping(value = ["/rawJson/{lobbyId}"])
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
     fun getGalleryJson(@PathVariable("lobbyId") lobbyId: String) : Gallery {
         println("GET rawJson/${lobbyId}, raw JSON call")
         return GalleryManager.loadFromFile(lobbyId)
